0ce232539650dfc1be0432462edb15b327fe482c
levpesin <lev.pesin666@gmail.com>
2017-12-01 16:11:05 -0600

    Strong Password Done

--- PasswordManager/src/Password.java
+++ PasswordManager/src/Password.java
@@ -1,4 +1,20 @@
 
 public class Password extends Accounts {
+	public static void strength() {
+        String password;
+        System.out.println("Create a Password (at least 8 characters");
+        password = myScan.next();
+        
+        if (password.length() <= 8) {
+        	System.out.println("Password is Too Short");
+        } else {
+        	if (isStrong(password)) {
+        		System.out.println("Very Strong");
+        	} else System.out.print("Weak");
+        }
+    }
+	private static boolean isStrong(String password){
+	    return password.matches("^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]");
 
+	  }
 }
