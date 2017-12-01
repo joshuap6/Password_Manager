e6154c0571060559f744de66c6cedb440f4f0e81
Josh <Josh@Joshua>
2017-12-01 16:24:42 -0600

    Update

--- PasswordManager/src/Accounts.java
+++ PasswordManager/src/Accounts.java
@@ -1,4 +1,39 @@
 
 public class Accounts extends Manager {
 	
+	private String name;
+	
+	private String username;
+	
+	private Password password;
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public static void strength() {
+        String password;
+        System.out.println("Create a Password (at least 8 characters");
+        password = myScan.next();
+        
+        if (password.length() <= 8) {
+        	System.out.println("Password is Too Short");
+        } else {
+        	if (isStrong(password)) {
+        		System.out.println("Very Strong");
+        	} else System.out.print("Weak");
+        }	
+        
+    }
+	private static boolean isStrong(String password){
+	    return password.matches("^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]");
+	}
+	
+	public String getPassword() {
+		return this.password.toString();
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
 }
--- PasswordManager/src/Manager.java
+++ PasswordManager/src/Manager.java
@@ -3,10 +3,46 @@ import java.util.Scanner;
 public class Manager {
 	static Scanner myScan;
 	
+	static Accounts[] accountList = new Accounts[50];
+	
 	public static void main(String[] args) {
-		System.out.println("_");
-		System.out.println("_");
+		myScan = new Scanner(System.in);
+		System.out.println("What would you like to do?");
+		System.out.println("[1] Store a new account");
+		System.out.println("[2] Retrieve an account");
+		System.out.println("[3] Exit program.");
+		int input = myScan.nextInt();
+		boolean isRunning = true;
+		while (isRunning == true) {
+			if (input == 1) {
+				
+			} else if (input == 2) {
+				System.out.println("Enter the name of the account:");
+				String inName = myScan.next();
+				Accounts inAccount = getAccount(inName);
+				if (inAccount == null) {
+					System.out.println("Sorry, that account was not found.");
+					System.out.println("Press enter to exit");
+				}
+			} else if (input > 3 || input < 1) {
+				System.out.println("Please input a valid integer");
+				input = myScan.nextInt();
+			} else {
+				isRunning = false;
+			}
+		}
 		myScan.close();
 	}
 	
+	public static Accounts getAccount(String accountName) {
+		for (int i = 0; i < accountList.length; i++) {
+			if (accountList[i] == null) {
+				return null;
+			}
+			if (accountList[i].getName() == accountName) {
+				return accountList[i];
+			}
+		}
+		return null;
+	}
 }
